c53e9d7e95772777e67d3fefbf9779f01ec2cf9c
kam_ <kamil.amrani@heig-vd.ch>
2018-10-05 11:08:41 +0200

    Main to launch both mais for testing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import main.java.Master;
+import main.java.Slave;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws InterruptedException {
+        Master.main(null);
+        Slave.main(null);
     }
 }
